ddc8f2e214d01f1bf0e07be85b212e376a0e8c49
39zde <168634234+39zde@users.noreply.github.com>
2024-06-26 14:19:40 +0000

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+import ScoringCalculator from "./src/js/ScoringCalculator.js";
+import CategoryRow from "./src/js/CategoryRow.js";
+
+customElements.define("scoring-calc", ScoringCalculator);
+customElements.define("scoring-calc-row", CategoryRow);
